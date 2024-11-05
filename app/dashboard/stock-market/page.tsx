3a15df92a1f07e199f70e487561341e35e601d63
sarahkwak <sarahkwak79@gmail.com>
2024-11-05 01:45:05 -0500

    setted up frontend

---
+++ app/dashboard/stock-market/page.tsx
@@ -0,0 +1,15 @@
+import StockMarket from "./StockMarket";
+
+const Page: React.FC = () => {
+  return (
+    <>
+      <StockMarket />
+    </>
+  );
+};
+
+export default Page;
+
+export const metadata = {
+  title: "Stock Market | GeeseHacks",
+};
