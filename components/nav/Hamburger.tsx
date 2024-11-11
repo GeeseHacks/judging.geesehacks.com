66cb3c61d107872e9cf0717f5ffb110c45b75629
sarahkwak <sarahkwak79@gmail.com>
2024-11-11 17:59:48 -0500

    prod build fix pt.4

--- components/nav/Hamburger.tsx
+++ components/nav/Hamburger.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import Image from "next/image";
 import Link from "next/link";
-import { signOutAction } from "@/utils/signOutAction";
+//import { signOutAction } from "@/utils/signOutAction";
 import { Button } from "@/components/ui/button";
 import { AiOutlineMenu, AiOutlineClose } from "react-icons/ai"; // Add icon library
 import { sideNavLinks, SideNavProps } from "./SideNav";
