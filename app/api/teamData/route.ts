ab911e5c49d51ec037afe0051a23b68bd7c3eb55
jenniferli8263 <75101197+jenniferli8263@users.noreply.github.com>
2025-01-09 22:36:07 -0500

    last try

--- app/api/teamData/route.ts
+++ app/api/teamData/route.ts
@@ -1,7 +1,8 @@
 import { NextRequest, NextResponse } from 'next/server';
 import prisma from '@lib/prisma';
 
-export async function GET(_: NextRequest) {
+export async function GET(req: NextRequest) {
+  console.log(req);
     try {
       const teams = await prisma.project.findMany();
       
