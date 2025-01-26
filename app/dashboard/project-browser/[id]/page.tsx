a1f039be901384d9a957f16c5a7ebe076db2a115
sarahkwak <sarahkwak79@gmail.com>
2025-01-25 22:00:18 -0500

    devpost added

--- app/dashboard/project-browser/[id]/page.tsx
+++ app/dashboard/project-browser/[id]/page.tsx
@@ -14,6 +14,7 @@ interface Project {
   yourInvestment: string;
   balance: string;
   projectMembers: string[];
+  devpostLink: string;
 }
 
 const ProjectDetails = () => {
@@ -144,6 +145,14 @@ const ProjectDetails = () => {
         <h1 className="text-2xl md:text-3xl font-semibold text-white">
           {project?.name}
         </h1>
+        <a 
+          href={project?.devpostLink} 
+          target="_blank" 
+          rel="noopener noreferrer" 
+          className="text-[#BD6CE6] hover:text-[#D175FA] transition-colors"
+        >
+          View on Devpost →
+        </a>
         <p className="sm:pb-6 text-sm sm:text-lg text-white break-words max-w-full sm:max-w-full md:max-w-full">
           {project?.description}
         </p>
